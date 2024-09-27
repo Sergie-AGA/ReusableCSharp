bd22af0ec1a19bb99d824e1d661214fecc44b3fb
Sergie <sergiearruda@gmail.com>
2024-09-27 17:45:39 +0100

    Project cleanup

--- obj/Debug/net8.0/ReusableCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReusableCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReusableCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65294076f85ac437aebb956bbaafb7ce29fca0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReusableCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReusableCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
