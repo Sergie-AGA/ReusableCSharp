bcad06932de176010769d70e0e24c6f621b222d0
Sergie <sergiearruda@gmail.com>
2024-10-02 14:07:39 +0100

    Basic post endpoint and code restructuring

---
+++ src/ReusableCSharp.Api/Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReusableCSharp.Communication.Requests;
+
+namespace ReusableCSharp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpPost]
+
+        public IActionResult Create([FromBody] RequestAddTodoJson request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/ReusableCSharp.Communication/Requests/RequestAddTodoJson.cs
@@ -0,0 +1,14 @@
+﻿namespace ReusableCSharp.Communication.Requests
+{
+    public class RequestAddTodoJson
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsCompleted { get; set; } = false;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+    }
+}
